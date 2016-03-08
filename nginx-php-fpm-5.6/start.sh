fd1be92ed30cac56a71903bd8e436e89dcddf288
Icedroid <shenggxhz@gmail.com>
2016-03-08 15:30:55 +0800

    fix bugs

--- nginx-php-fpm-5.6/start.sh
+++ nginx-php-fpm-5.6/start.sh
@@ -1,3 +1,7 @@
 #/bin/sh
-php-fpm
-nginx -g daemon off
+
+# Again set the right permissions (needed when mounting from a volume)
+chown -Rf www-data.www-data /opt/htdocs/
+
+# Start supervisord and services
+/usr/bin/supervisord -n -c /etc/supervisord.conf
