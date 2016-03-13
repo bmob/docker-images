fd734294d6f73d076ddb282377da6fc3d67ef5d0
Icedroid <shenggxhz@gmail.com>
2016-03-13 18:00:25 +0800

    add php 5.5 and 7.0

---
+++ nginx-php-fpm-5.5/scripts/start.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+# Again set the right permissions (needed when mounting from a volume)
+chown -Rf www-data.www-data /opt/htdocs/
+# Start supervisord and services
+/usr/bin/supervisord -n -c /etc/supervisord.conf
