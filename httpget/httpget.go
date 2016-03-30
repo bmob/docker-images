4336d8b4dec37357d305d6897bc71a96e73eb276
Icedroid <shenggxhz@gmail.com>
2016-03-30 19:33:27 +0800

    add net limit httpget command

---
+++ httpget/httpget.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		return
+	}
+
+	APPID := os.Getenv("APPID")
+	BANDWIDTH := os.Getenv("BANDWIDTH")
+
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		req := fmt.Sprintf("%s?ip=null&appid=%s&bindwidth=%s", os.Args[1], APPID, BANDWIDTH)
+		http.Get(req)
+		os.Exit(1)
+	}
+
+	var ip string
+	ip = ""
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ip = ipnet.IP.String()
+			}
+
+		}
+	}
+
+	req := fmt.Sprintf("%s?ip=%s&appid=%s&bindwidth=%s", os.Args[1], ip, APPID, BANDWIDTH)
+	http.Get(req)
+}
