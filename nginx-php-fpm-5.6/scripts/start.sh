ab89ac46d67f04008b2e346fbb113ba2a718053b
Icedroid <shenggxhz@gmail.com>
2016-03-31 10:13:24 +0800

    add httpget

--- nginx-php-fpm-5.6/scripts/start.sh
+++ nginx-php-fpm-5.6/scripts/start.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 # Again set the right permissions (needed when mounting from a volume)
+/usr/bin/httpget http://api.bmobapp.com/1/netlimit
 chown -Rf www-data.www-data /opt/htdocs/
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
-/usr/bin/httpget http://api.bmobapp.com/1/netlimit
