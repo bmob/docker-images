b0b6705b2dfeccf14e707b2c10468e33314695d1
newjueqi <h6k65@126.com>
2016-05-09 19:16:53 +0800

    add an runable image

--- nodejs/scripts/start.sh
+++ nodejs/scripts/start.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 # Again set the right permissions (needed when mounting from a volume)
 /usr/bin/httpget http://10.45.35.148:8080/1/netlimit &
-chown -R 777 /opt/htdocs/
-# Start supervisord and services
-/usr/bin/supervisord -n -c /etc/supervisord.conf
 # start nodejs process
-/usr/local/bin/pm2 /opt/htdocs/app.js -i max 
+/usr/local/bin/pm2 start /opt/htdocs/app.js -i max
+# Start supervisord and service
+echo "start supervisord11"
+/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
